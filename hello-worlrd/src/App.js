9906b1abd9fc6eea857df289dcc23370d895a2cb
muindemwangangi2@gmail.com <emwangangi@jambopay.com>
2023-09-28 06:29:39 +0300

    arrow and class based components

---
+++ hello-worlrd/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import  Greet from './Components/Greet'
+
+function App() {
+  return (
+    <div className="App">
+     <Greet/>
+    </div>
+  );
+}
+
+export default App;
+
+
---
+++ hello-worlrd/src/Components/Greet.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+// function Greet() {
+//     return <p>hello Mwangangi</p>
+// }
+
+// arrow syntax
+
+ const Greet = () => {
+    return <p>Hello Mwangangi 2</p>
+}
+
+
+export default Greet
+
+// named export 
+// default export
